93571470ddd86b796bf6f5fd27f806ae3891ba50
Maxine <maxine.armstrong2005@gmail.com>
2024-10-12 14:59:45 +0100

    asteroid polygons

---
+++ .history/app/(tabs)/scores_20241012154248.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useGame } from '../../contexts/GameContext';
+
+let highScoreList: number[] = [100, 200, 300, 400, 500]; //placeholder scores
+
+export default function ScoresScreen() {
+  const { highScore } = useGame();
+
+  return (
+    <View style={styles.container}>
+      <View className>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+    marginBottom: 20,
+  },
+  score: {
+    fontSize: 48,
+    color: '#fff',
+  },
+});
